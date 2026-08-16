7ccc56c3ce69dfe2c3cb8f160a6887c9f83ca656
Niki182 <nithika.dhyan@gmail.com>
2026-08-16 16:46:14 +0530

    fix loading-circle.js

--- loading-circle.js
+++ loading-circle.js
@@ -1,8 +1,11 @@
 window.addEventListener('load', () => {
     const loader = document.querySelector('.loader');
+    
+    if (!loader) return;
 
     loader.classList.add('loader-hidden');
+    
     loader.addEventListener('transitionend', () => {
-        document.body.removeChild(loader);
-    })
-})
+        loader.remove();
+    }, { once: true });
+});
