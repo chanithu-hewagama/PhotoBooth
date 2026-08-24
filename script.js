90201f8e7b4e56a268984ff103f75b348801baf9
Niki182 <nithika.dhyan@gmail.com>
2026-08-24 16:49:57 +0530

    Lol

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const settingsBtn = document.getElementById('settings-trigger');
 const dropdownMenu = document.getElementById('dropdown-menu');
 
+dropdownMenu.style.display = 'none';
+
 settingsBtn.addEventListener('click', (event) => {
   event.stopPropagation();
   dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
