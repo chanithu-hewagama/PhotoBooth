fd4c2e3ac9a94ed40807c019e47bdf309217ec6e
Niki182 <nithika.dhyan@gmail.com>
2026-08-24 16:54:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ settingsBtn.addEventListener('click', (event) => {
   dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 });
 
+dropdownMenu.addEventListener('click', (event) => {
+  event.stopPropagation();
+  
+  if (event.target.closest('.goog-te-gadget') || event.target.closest('a') || event.target.closest('span')) {
+    if (event.target.id !== 'google_translate_element') {
+      dropdownMenu.style.display = 'none';
+    }
+  }
+});
+
 document.addEventListener('click', () => {
   dropdownMenu.style.display = 'none';
 });
